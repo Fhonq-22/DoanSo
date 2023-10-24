24478cb7c79d0a3eb2a5674a05acfa68e8605e26
Phòng <113459855+Fhonq-22@users.noreply.github.com>
2023-10-24 17:01:36 +0700

    Update script_themHieuungPhaoHoa.js
    Thêm hiệu ứng pháo hoa khi bingo

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (soChuSoDungViTri === 4) {
             chinhxac.play(); // Phát âm thanh
+            hienThiPhaoHoaKhiBingo(); // Gọi hàm hiển thị pháo hoa
             document.getElementById("ket_qua").innerHTML = `<span style="color: green;">Bingo! Mục tiêu là: ${mucTieu}</span>`;
             soDoanSubmit.disabled = true;
         } else if (doanCount >= soLanDoanMAX) {
@@ -135,4 +136,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
         soDoanInput.value = "";
     }
+
+    function hienThiPhaoHoa() {
+        const firework = document.createElement("div");
+        firework.classList.add("firework");
+
+        // Tạo màu ngẫu nhiên dưới dạng mã màu hex (#RRGGBB)
+        const randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+        firework.style.backgroundColor = randomColor;
+        
+        // Đặt vị trí ngẫu nhiên cho pháo hoa giấy
+        const leftPosition = Math.random() * window.innerWidth;
+        const topPosition = Math.random() * window.innerHeight;
+        firework.style.left = leftPosition + "px";
+        firework.style.top = topPosition + "px";
+
+        document.body.appendChild(firework);
+
+        setTimeout(() => {
+            // Loại bỏ pháo hoa giấy sau khi hoàn thành hiệu ứng
+            document.body.removeChild(firework);
+        }, 3000); // Thời gian tồn tại của pháo hoa giấy (1 giây)
+    }
+
+    function hienThiPhaoHoaKhiBingo() {
+        for (let i = 0; i < 22; i++) {
+            setTimeout(() => {
+                hienThiPhaoHoa();
+            }, i * 100); // Mỗi pháo hoa giấy xuất hiện sau 100ms
+        }
+    }
+    
 });
